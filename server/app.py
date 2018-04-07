4dbd05a557cbbca5b5ab085bef9d2972a96e26ea
Patrice Desrochers <despat-96@outlook.com>
2018-04-07 11:29:26 -0400

    governement start

--- server/app.py
+++ server/app.py
@@ -10,11 +10,13 @@ parser = reqparse.RequestParser()
 
 person = {
     '1': {
+        'name':"Itch",
         'perso': "Enginering",
         'tasks': [],
         'count': 0
     },
     '2': {
+        'name':"Bob",
         'perso': 'Doctor',
         'tasks': [],
         'count': 0
@@ -34,39 +36,46 @@ tasks = {
 
 asignTask = []
 
+messages = {
+    '1':{
+        'message':"Server start"}
+}
 
-def CreateTask(tasksJson):
-    task = {}
-    
-    task['name'] = tasksJson['name']
-    return task
+#def ClearMessages():
+#    nbr = int(max(messages.keys()))
+#    lastMsg = messages[str(nbr)]
+#    messages.clear()
+#    messages['1'] = lastMsg
+
+def RecordMessage(msg):
+    nbr = int(max(messages.keys(),key=int))+1
+    messages[str(nbr)] = {'message':msg}
 
 def GetRandomTask():
-    nbMax = int(max(tasks.keys()))
+    nbMax = int(max(tasks.keys(),key=int))
     if(len(asignTask) < nbMax):
         ran = random.randint(1, nbMax)
         task = tasks[str(ran)]
-        while(task in asignTask):
+        while(ran in asignTask):
             ran = random.randint(1, nbMax)
             task = tasks[str(ran)]
-        asignTask.append(task)
+        asignTask.append(ran)
         return task
     else:
         return None
 
-
-class Personalities(Resource):
+class Persons(Resource):
     def get(self):
         return person
 
     def post(self):
-        #args = parser.parse_args()
         json_data = request.get_json(force=True)
-        p = {'perso': json_data['perso'], 'tasks': json_data['tasks'],'count':0}
-        person_id = int(max(person.keys()))+1
+        p = {'name':json_data['name'], 'perso': json_data['perso'], 'tasks': json_data['tasks'],'count':0}
+        print(max(person.keys(),key=int))
+        person_id = int(max(person.keys(),key=int))+1
         person[person_id] = p
-        return person
-
+        RecordMessage("Added person " + p['name'])
+        return person[person_id]
 
 class Tasks(Resource):
     def get(self):
@@ -82,23 +91,30 @@ class OnePerson(Resource):
         p['count'] = p['count']+1
         ptask = p['tasks']
         task_id = p['count']
-        task = CreateTask(json_data)
-        asignTask.remove(task)
-        comp = {str(task_id): task}
+        idFinish = int(json_data['id'])
+        asignTask.remove(idFinish)
+        comp = {str(task_id): tasks[str(idFinish)]}
         ptask.append(comp)
+        RecordMessage("Task " +  tasks[str(idFinish)]['name'] + " has been finished by " + p['name'])
         return person[person_id]
-
 
 class OneTask(Resource):
     def get(self):
         task = GetRandomTask()
+        RecordMessage("Task " + task['name'] + " has been give")
         return task
 
+class Governement(Resource):
+    def get(self):
+        msg= messages
+       # ClearMessages()
+        return msg
 
-api.add_resource(Personalities, '/perso')
+api.add_resource(Persons, '/persons')
 api.add_resource(Tasks, '/tasks')
-api.add_resource(OnePerson, '/perso/<person_id>')
+api.add_resource(OnePerson, '/persons/<person_id>')
 api.add_resource(OneTask, '/tasks/next')
+api.add_resource(Governement,'/gov')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5002, debug=True)
