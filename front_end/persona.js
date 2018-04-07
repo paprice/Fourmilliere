0a89a938914aa938ac02a9748ca01227ed833ba1
Karl <ritchie.karl@gmail.com>
2018-04-07 17:46:53 -0400

    version qui fonctionne

--- front_end/persona.js
+++ front_end/persona.js
@@ -1,7 +1,12 @@
 
 random = Math.floor((Math.random() * 5) + 1);
 
-personas = ['duty fulfiller', 'mechanic', 'nurturer', 'thinker', 'scientist'];
+personas = ['duty_fullfiller', 'mechanic', 'nurturer', 'thinker', 'scientist'];
+
+ID = -1;
+
+total_coins = 0;
+last_weight = 0;
 
 function load_random_persona(){
     persona = personas[random - 1];
@@ -13,7 +18,7 @@ function load_random_persona(){
     xhr.onload = function (e) {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                console.log(xhr.responseText);
+                ID = xhr.responseText;
             } else {
                 console.error(xhr.statusText);
             }
@@ -22,6 +27,61 @@ function load_random_persona(){
     // xhr.onerror = function (e) {
     // console.error(xhr.statusText);
     // };
-    xhr.send({"name": "a", "perso": "a"});
+    xhr.send('{"name": "Steve", "perso": "' + persona + '"}');
 
+}
+
+
+function get_tasks(){
+    if (ID == -1){
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://172.20.34.161:5002/persons/"+ID+"/next", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                tasks = JSON.parse(xhr.responseText);
+                last_weight = tasks['weight'];
+                last_task = tasks['name']
+                document.getElementById('tasks').innerHTML = '<p>You have to : ' + tasks['name'] + '</p>';
+                document.getElementById('tasks').innerHTML += '<p>By doing doing this, you would gain ' + tasks['weight'] + ' challenge coins.'
+                document.getElementById('tasks').innerHTML += '<button onclick="send_tasks()">Complete a task !</button>';
+            } else {
+                console.error(xhr.statusText);
+            }
+        }
+    };
+    // xhr.onerror = function (e) {
+    // console.error(xhr.statusText);
+    // };
+    xhr.send(null);
+
+}
+
+function send_tasks(){
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://172.20.34.161:5002/persons/"+ID, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                ID = xhr.responseText;
+                total_coins += last_weight;
+                alert('Congratulations ! Your current challenge coins balance is ' + total_coins);
+                document.getElementById('tasks').innerHTML = '<button onclick="get_tasks()">Get Tasks !</button>';
+            } else {
+                console.error(xhr.statusText);
+            }
+        }
+    };
+    // xhr.onerror = function (e) {
+    // console.error(xhr.statusText);
+    // };
+    xhr.send('{"name": "' + last_task + '"}');
+
+    
 }
