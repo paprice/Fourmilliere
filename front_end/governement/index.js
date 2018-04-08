f8eb13c80fbed3af21544d9268516dd69e8ee23b
Patrice Desrochers <despat-96@outlook.com>
2018-04-08 11:26:28 -0400

    modif gouvernement

--- front_end/governement/index.js
+++ front_end/governement/index.js
@@ -20,7 +20,7 @@ function GetMessages() {
                     document.getElementById('record').innerHTML += messages['msg'][i]['message'] + "<br/>"
                 }
 
-                DeleteMessages()    
+                DeleteMessages()
             } else {
                 console.error(xhr.statusText);
             }
@@ -46,7 +46,7 @@ function DeleteMessages() {
     xhr.send(null);
 }
 
-function ResetTasks(){
+function ResetTasks() {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", address + "/tasks", true);
     xhr.setRequestHeader("Content-type", "application/json");
@@ -54,6 +54,66 @@ function ResetTasks(){
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
 
+            } else {
+                console.error(xhr.statusText);
+            }
+        }
+    };
+    xhr.send(null);
+}
+
+function GetColonists() {
+    document.getElementById('colonists').innerHTML = ""
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", address + "/persons", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var messages = JSON.parse(xhr.responseText);
+                for (var i in messages) {
+                    var key = i;
+                    var val = messages[i];
+                    for (var j in val) {
+                        var sub_key = j;
+                        var sub_val = val[j];
+                        if (sub_key == "name") {
+                            document.getElementById('colonists').innerHTML += sub_val 
+                        }
+                        if (sub_key == "coin") {
+                            document.getElementById('colonists').innerHTML += '<font color="blue"> coin : '+ sub_val + '<font/><br/>'
+                        }
+
+                    }
+                }
+            } else {
+                console.error(xhr.statusText);
+            }
+        }
+    };
+    xhr.send(null);
+}
+
+function GetTasks() {
+    document.getElementById('tasks').innerHTML = ""
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", address + "/tasks", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var messages = JSON.parse(xhr.responseText);
+                for (var i in messages) {
+                    var key = i;
+                    var val = messages[i];
+                    for (var j in val) {
+                        var sub_key = j;
+                        var sub_val = val[j];
+                        if (sub_key == "name") {
+                            document.getElementById('tasks').innerHTML += sub_val + "<br/>"
+                        }
+                    }
+                }
             } else {
                 console.error(xhr.statusText);
             }
